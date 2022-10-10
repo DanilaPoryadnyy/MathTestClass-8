985b29989ec29a6dc7df01d128f9bacc1f9b4d39
DO <nozoleg@mail.ru>
2022-10-10 15:34:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Geometry
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public int aaa(int b,int a)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp12;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            Geometry g = new Geometry();
+            int actual = g.aaa(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
